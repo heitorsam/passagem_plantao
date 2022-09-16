80458e2fc84d57e32ad1543f6fe0ef5cb10faaa5
CaioBecker <83302532+CaioBecker@users.noreply.github.com>
2022-09-16 14:03:50 -0300

    Update ajax_tabela_plantao_r.php

--- funcoes/plantao/ajax_tabela_plantao_r.php
+++ funcoes/plantao/ajax_tabela_plantao_r.php
@@ -12,7 +12,7 @@
     $ck_temp = $_GET['sn_reserva'];
 
     $con_exibir_paciente="SELECT lt.ds_leito,
-                                    TO_CHAR(mi.hr_mov_int, 'DD/MM/YYYY'),
+                                    TO_CHAR(mi.hr_mov_int, 'DD/MM/YYYY HH24:MI') as entrada,
                                     CASE
                                     WHEN atd.cd_atendimento IS NULL THEN
                                     'RESERVA SEM ATENDIMENTO'
@@ -22,12 +22,12 @@
                                     pac.TP_SEXO,
                                     pac.CD_PACIENTE,
                                     FLOOR((SYSDATE - pac.DT_NASCIMENTO) / 365.242199) AS IDADE,
-                                    pac.NM_MAE,
                                     lt.CD_LEITO,
                                     lt.DS_RESUMO,
                                     unid.DS_UNID_INT,
                                     lt.CD_UNID_INT,
-                                    mi.cd_atendimento
+                                    mi.cd_atendimento,
+                                    esp.ds_especialid
                                 FROM dbamv.LEITO lt
                                 INNER JOIN dbamv.MOV_INT mi
                                 ON mi.cd_leito = lt.cd_leito
@@ -37,6 +37,8 @@
                                 ON pac.cd_paciente = atd.cd_paciente
                                 INNER JOIN dbamv.UNID_INT unid
                                 ON unid.CD_UNID_INT = lt.CD_UNID_INT
+                                INNER JOIN dbamv.especialid esp
+                                ON esp.cd_especialid = atd.cd_especialid
                                 WHERE mi.tp_mov = 'R'
                                 AND lt.cd_unid_int = $var_exibir_pp
                                 AND lt.tp_ocupacao = 'R'
@@ -62,12 +64,13 @@
 
             <tr>
 
+                <th style="text-align: center;">Entrada Prevista</th>
                 <th style="text-align: center;">Atendimento</th>
                 <th style="text-align: center;">Prontuario</th>
                 <th style="text-align: center;">Paciente</th>
                 <th style="text-align: center;">Sexo</th>
                 <th style="text-align: center;">Idade</th>
-                <th style="text-align: center;">Mãe</th>
+                <th style="text-align: center;">Especialidade</th>
                 <th style="text-align: center;">Resumo</th>
                 <th style="text-align: center;">Unidade de Internação</th>
                 <th style="text-align: center;">Ações</th>
@@ -84,12 +87,13 @@
 
                 echo'</tr>';
 
+                echo '<td class="align-middle" style="text-align: center;">' . $row_exibir_pac['ENTRADA'] . '</td>';
                 echo '<td class="align-middle" style="text-align: center;">' . @$row_exibir_pac['CD_ATENDIMENTO'] . '</td>';
                 echo '<td class="align-middle" style="text-align: center;">' . $row_exibir_pac['CD_PACIENTE'] . '</td>';
                 echo '<td class="align-middle" style="text-align: center;">' . $row_exibir_pac['NM_PACIENTE'] . '</td>';
                 echo '<td class="align-middle" style="text-align: center;">' . $row_exibir_pac['TP_SEXO'] . '</td>';
                 echo '<td class="align-middle" style="text-align: center;">' . $row_exibir_pac['IDADE'] . '</td>';
-                echo '<td class="align-middle" style="text-align: center;">' . $row_exibir_pac['NM_MAE'] . '</td>';
+                echo '<td class="align-middle" style="text-align: center;">' . $row_exibir_pac['DS_ESPECIALID'] . '</td>';
                 echo '<td class="align-middle" style="text-align: center;">' . $row_exibir_pac['DS_RESUMO'] . '</td>';
                 echo '<td class="align-middle" style="text-align: center;">' . $row_exibir_pac['DS_UNID_INT'] . '</td>';
                 echo '<td class="align-middle" style="text-align: center;">';  
