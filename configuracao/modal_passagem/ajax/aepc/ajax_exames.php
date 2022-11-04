c15e9fcda3c10dacdf808aa52da2734bd216f7a6
CaioBecker <83302532+CaioBecker@users.noreply.github.com>
2022-11-04 09:57:00 -0300

    ajuste

--- configuracao/modal_passagem/ajax/aepc/ajax_exames.php
+++ configuracao/modal_passagem/ajax/aepc/ajax_exames.php
@@ -80,7 +80,6 @@
                             AND pm.CD_ATENDIMENTO = $var_atd
                             AND itrx.CD_LAUDO IS NULL
                             AND itrx.sn_realizado = 'N'
-                            AND pm.dt_validade > sysdate
                             ORDER BY status ,itrx.dt_entrega desc";
 
         
--- funcoes/plantao/ajax_tabela_plantao.php
+++ funcoes/plantao/ajax_tabela_plantao.php
@@ -62,7 +62,7 @@
                                         ON uni.CD_UNID_INT = lt_set.CD_UNID_INT
                                     INNER JOIN dbamv.especialid esp
                                         ON esp.cd_especialid = atd.cd_especialid
-                                    WHERE lt_set.CD_UNID_INT = 1
+                                    WHERE lt_set.CD_UNID_INT = $var_exibir_pp
                                         AND TRUNC(TO_DATE('$var_exibir_dt', 'YYYY-MM-DD')) BETWEEN 
                                             TRUNC(lt_set.DT_ENTRADA) AND TRUNC(lt_set.DT_SAIDA)
                 
